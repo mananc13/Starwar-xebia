649559333cb2bef03f5138ebb44a3c491caf9be5
Manan Chawla <mananchawla@Manans-MacBook-Air.local>
2020-11-21 23:34:10 +0530

    Updates

---
+++ precache-manifest.db9431774d32f663203eb6379c741ee3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e441c3e4ecd5ad125a8ae806a5825d3e",
+    "url": "/Starwar-xebia/index.html"
+  },
+  {
+    "revision": "540bfefb435533b5a004",
+    "url": "/Starwar-xebia/static/css/3.b883f25b.chunk.css"
+  },
+  {
+    "revision": "5a66e4e64151fbabaa5e",
+    "url": "/Starwar-xebia/static/css/4.95323df8.chunk.css"
+  },
+  {
+    "revision": "6a14dec58226f8a8390b",
+    "url": "/Starwar-xebia/static/css/main.2f6e7198.chunk.css"
+  },
+  {
+    "revision": "3517474bb1c48d2f5b94",
+    "url": "/Starwar-xebia/static/js/2.54ce952f.chunk.js"
+  },
+  {
+    "revision": "540bfefb435533b5a004",
+    "url": "/Starwar-xebia/static/js/3.7bf5347c.chunk.js"
+  },
+  {
+    "revision": "5a66e4e64151fbabaa5e",
+    "url": "/Starwar-xebia/static/js/4.dfe9343c.chunk.js"
+  },
+  {
+    "revision": "6a14dec58226f8a8390b",
+    "url": "/Starwar-xebia/static/js/main.620b4a33.chunk.js"
+  },
+  {
+    "revision": "9c6f101c38932b249a89",
+    "url": "/Starwar-xebia/static/js/runtime~main.67fe6bae.js"
+  }
+]);
